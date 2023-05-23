3f436191796a257f777ed68d4431898b7caab72a
Nat <anhht9x@gmail.com>
2023-05-23 09:55:30 +0700

    .

---
+++ app/src/main/java/nat/pink/base/ui/notification/NotificationFragment.java
@@ -0,0 +1,25 @@
+package nat.pink.base.ui.notification;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import nat.pink.base.base.BaseFragment;
+import nat.pink.base.databinding.FragmentSetupNotificationBinding;
+import nat.pink.base.ui.home.HomeViewModel;
+
+public class NotificationFragment extends BaseFragment<FragmentSetupNotificationBinding, HomeViewModel> {
+    @Override
+    protected HomeViewModel getViewModel() {
+        return new ViewModelProvider(this).get(HomeViewModel.class);
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+}
