f622d9d6ac2cf801197962b4ca39ac29072bfaf3
Nat <anhht9x@gmail.com>
2023-05-24 16:48:42 +0700

    .

--- app/src/main/java/nat/pink/base/ui/create/CreateUserFragment.java
+++ app/src/main/java/nat/pink/base/ui/create/CreateUserFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 
+import nat.pink.base.R;
 import nat.pink.base.base.BaseFragment;
 import nat.pink.base.databinding.FragmentCreateUserBinding;
 import nat.pink.base.ui.home.HomeViewModel;
@@ -26,18 +27,26 @@ public class CreateUserFragment extends BaseFragment<FragmentCreateUserBinding, 
     }
 
     @Override
+    protected void initView() {
+        super.initView();
+        binding.llTop.txtTitle.setText(getString(R.string.create_your_contact));
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+
+    @Override
     protected void initEvent() {
         super.initEvent();
-        binding.swStatus.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
-                    binding.lineView1.setVisibility(View.VISIBLE);
-                    binding.radios.setVisibility(View.VISIBLE);
-                }else{
-                    binding.lineView1.setVisibility(View.GONE);
-                    binding.radios.setVisibility(View.GONE);
-                }
+        binding.swStatus.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked){
+                binding.lineView1.setVisibility(View.VISIBLE);
+                binding.radios.setVisibility(View.VISIBLE);
+            }else{
+                binding.lineView1.setVisibility(View.GONE);
+                binding.radios.setVisibility(View.GONE);
             }
         });
 
