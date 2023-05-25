72cc24c4385d972f5f7337250314be8a3d24eabe
Nat <anhht9x@gmail.com>
2023-05-25 09:59:20 +0700

    .

--- app/src/main/java/nat/pink/base/ui/home/HomeFragment.java
+++ app/src/main/java/nat/pink/base/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package nat.pink.base.ui.home;
 
+import android.annotation.SuppressLint;
 import android.view.View;
 
 import androidx.core.view.GravityCompat;
@@ -12,16 +13,14 @@ import nat.pink.base.R;
 import nat.pink.base.adapter.AdapterFakeUser;
 import nat.pink.base.base.BaseFragment;
 import nat.pink.base.databinding.HomeFragmentBinding;
-import nat.pink.base.dialog.DialogNetworkFail;
 import nat.pink.base.dialog.DialogSelectChat;
 import nat.pink.base.ui.notification.NotificationFragment;
 import nat.pink.base.utils.Const;
-import nat.pink.base.utils.InAppPurchase;
-import nat.pink.base.utils.Utils;
 
 public class HomeFragment extends BaseFragment<HomeFragmentBinding, HomeViewModel> {
     private AdapterFakeUser adapterFakeUser;
     public static final String TAG = "HomeFragment";
+    private DialogSelectChat dialog;
 
     @Override
     protected HomeViewModel getViewModel() {
@@ -38,6 +37,13 @@ public class HomeFragment extends BaseFragment<HomeFragmentBinding, HomeViewMode
         ln.setOrientation(RecyclerView.HORIZONTAL);
         binding.rcvFakeUser.setLayoutManager(ln);
         binding.rcvFakeUser.setAdapter(adapterFakeUser);
+
+        dialog = new DialogSelectChat(requireContext(), R.style.MaterialDialogSheet, user -> {
+            if (user.getId() == -1) {
+                addFragment(new CreateUserFragment(), CreateUserFragment.TAG);
+                dialog.dismiss();
+            }
+        });
     }
 
     @Override
@@ -56,7 +62,7 @@ public class HomeFragment extends BaseFragment<HomeFragmentBinding, HomeViewMode
             binding.drawerLayout.openDrawer(GravityCompat.END);
         });
         binding.fakeMessage.setOnClickListener(v -> {
-            showAds(Const.KEY_ADS_MESSAGE);
+            showAction(Const.KEY_ADS_MESSAGE);
         });
         binding.fakeNoti.setOnClickListener(v->{
             addFragment(new NotificationFragment(),NotificationFragment.TAG);
@@ -64,44 +70,8 @@ public class HomeFragment extends BaseFragment<HomeFragmentBinding, HomeViewMode
 
     }
 
-    private void showAds(String key) {
-        if (Utils.isNetworkAvailable(requireActivity())) {
-            if (isHideAds() || InAppPurchase.getInstance().isPurchaseAdsRemove()) {
-                showAction(key);
-                return;
-            }
-            Utils.showDialogRemoveAds(requireActivity(), o -> {
-                binding.frAdsHome.setVisibility(View.GONE);
-            }, o -> {
-                setInterstitialAdUnitId(key);
-                initInterstitialAd();
-            });
-        } else {
-            new DialogNetworkFail(requireContext(), R.style.MaterialDialogSheet, false).show();
-        }
-    }
-
-    private DialogSelectChat dialog;
-
     private void showMessage() {
         binding.frAdsHome.setVisibility(View.VISIBLE);
-        //  App.firebaseAnalytics.logEvent("Access_Message_Function", null);
-        //  Toast.makeText(this.getContext(), "hello ", Toast.LENGTH_SHORT).show();
-        //  ((MainActivity) getActivity()).addChildFragment(new FragmentListUser(), FragmentListUser.class.getSimpleName());
-
-        dialog = new DialogSelectChat(requireContext(), R.style.MaterialDialogSheet, user -> {
-            if (user.getId() == -1) {
-                //create mew user
-                //    ((MainActivity) getActivity()).addFragment(new CreateUserFragment(), CreateUserFragment.TAG);
-                addFragment(new CreateUserFragment(), CreateUserFragment.TAG);
-                dialog.dismiss();
-            }
-
-        });
-//        ColorDrawable back = new ColorDrawable(Color.TRANSPARENT);
-//        InsetDrawable inset = new InsetDrawable(back, 100);
-//        dialog.getWindow().setBackgroundDrawable(inset);
-        // dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         dialog.show();
     }
 
