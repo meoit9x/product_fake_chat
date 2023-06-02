fea386bcac3b6a4a2c77c18cc67470ce756bb7ef
Nat <anhht9x@gmail.com>
2023-06-02 14:42:05 +0700

    .

--- app/src/main/java/nat/pink/base/ui/splah/SplashActivity.java
+++ app/src/main/java/nat/pink/base/ui/splah/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -24,6 +25,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         binding = ActivitySplashBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         initAct();
@@ -40,6 +43,7 @@ public class SplashActivity extends AppCompatActivity {
                     Intent i = new Intent(getApplicationContext(), MainActivity.class);
                     i.setAction("android.intent.action.MAIN");
                     startActivity(i);
+                    finish();
                 }
             }
         };
