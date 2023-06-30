6022c92969fe3467c6b473fbbd5037a118857e9f
Nat <anhht9x@gmail.com>
2023-06-30 15:47:37 +0700

    .

--- app/src/main/java/nat/pink/base/base/BaseActivityForFragment.java
+++ app/src/main/java/nat/pink/base/base/BaseActivityForFragment.java
@@ -248,15 +248,15 @@ public abstract class BaseActivityForFragment extends AppCompatActivity {
     }
 
     public void createNativeAd(String keyAds) {
-        setLoadingAdsView(true);
+//        setLoadingAdsView(true);
         adLoader = new AdLoader.Builder(this, keyAds).forNativeAd(nativeAd -> {
             consumerAdsSuccess.accept(nativeAd);
-            setLoadingAdsView(false);
+//            setLoadingAdsView(false);
         }).withAdListener(new AdListener() {
             @Override
             public void onAdFailedToLoad(LoadAdError adError) {
                 consumerAdsFaill.accept(keyAds);
-                setLoadingAdsView(false);
+//                setLoadingAdsView(false);
             }
         }).withNativeAdOptions(new NativeAdOptions.Builder()
                 .build()).build();
